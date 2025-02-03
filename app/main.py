79627f1415efd63d42e50f697020703158d166b9
Seunghoon Shin <liante0904@users.noreply.github.com>
2025-02-03 10:55:40 +0900

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/reports/global/")
+def report_global():
+    return {"report": "Global Report"}
